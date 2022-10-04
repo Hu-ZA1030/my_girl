47711c689079dacf4cf2253de7fa9877aaf8e51a
Hu-ZA1030 <2250552567@qq.com>
2022-10-04 23:53:27 +0800

    删除build:test的命令

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -1,9 +1,19 @@
+/*
+ * @Author: Hu-ZA1030 2250552567@qq.com
+ * @Date: 2022-10-02 15:48:49
+ * @LastEditors: Hu-ZA1030 2250552567@qq.com
+ * @LastEditTime: 2022-10-04 17:14:26
+ * @FilePath: \my_girl\src\config\config.test.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by Hu-ZA1030 2250552567@qq.com, All Rights Reserved. 
+ */
 // 【测试环境】的应用配置文件
 export default {
   // 前端路由的根路径
-  webBaseURL: '/an:ntsapps_dev_m/mhrcms/',
+  // webBaseURL: '/an:ntsapps_dev_m/mhrcms/',
   // 后台api默认前缀
-  apiBaseURL: '/an:ntsapps_dev_m/mhrcms/api',
+  // apiBaseURL: '/an:ntsapps_dev_m/mhrcms/api',
   // 是否是开发环境
   isDev: false,
   // 是否开启VC
